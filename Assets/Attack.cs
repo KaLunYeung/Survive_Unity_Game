987f9c464071a412a6751c39e8571382ff444cfa
Ka Lun Yeung <kalunyeung6@gmail.com>
2020-08-23 14:39:19 -0700

    Revert "Revert "Revert "Survive_Game"""
    This reverts commit 8be31cfac6e290da8a6a15195d5a90cfd743a301.

---
+++ Assets/Attack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            
+            Destroy(this.gameObject);
+
+
+
+            
+            Destroy(collision.gameObject);
+            
+
+        }
+
+        else if (collision.gameObject.tag == "Player")
+        {
+            
+
+
+
+
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
---
+++ Assets/BulletLaser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLaser : MonoBehaviour
+{
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+
+            Destroy(this.gameObject, 0.5f);
+
+
+            collision.gameObject.GetComponent<HealthSystem>().TakeDamage(5);
+            if (collision.gameObject.GetComponent<HealthSystem>().currentHealth <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+
+        }
+
+    }
+}
