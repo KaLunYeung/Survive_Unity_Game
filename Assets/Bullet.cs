f1bd82c44ef9bb4997bea21e4199eb8e2ff6dd87
Ka Lun Yeung <kalunyeung6@gmail.com>
2020-08-23 14:18:29 -0700

    Revert "Survive_Game"
    This reverts commit 1673f889baf8d69294cc7fa43123a4da18e67e60.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+
+            Destroy(this.gameObject);
+
+
+            collision.gameObject.GetComponent<HealthSystem>().TakeDamage(1);
+            if (collision.gameObject.GetComponent<HealthSystem>().currentHealth <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+
+        }
+        
+    }
+}
