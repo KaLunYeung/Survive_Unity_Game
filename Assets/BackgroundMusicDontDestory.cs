db5c2af0c210c95477db58b475826d91b4f8912f
Ka Lun Yeung <kalunyeung6@gmail.com>
2020-08-25 17:09:14 -0700

    Added Background Music

---
+++ Assets/BackgroundMusicDontDestory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicDontDestory : MonoBehaviour
+{
+
+    static BackgroundMusicDontDestory instance = null;
+
+    
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    
+}
